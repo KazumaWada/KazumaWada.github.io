110b7cf12279be95bd196ab44028e31f27a9ed61
Kazuma Wada <kazumawada.aus@gmail.com>
2024-11-23 21:44:48 +0700

    prototype04

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,11 +4,11 @@
      <h3>🧑‍💻 Projects</h3>
   <div>
   <li>🚀 <a href="https://travelpackhub.com" target="_blank">travelpackhub.com</a> ,<a href="https://github.com/KazumaWada/travelpackhub" target="_blank"><i class="fa-brands fa-github"></i></a></li>
-  <li>🚀 <a href="https://thisjobdoesnotexistyet.com" target="_blank">thisjobdoesnotexistyet.com</a> ,<a href="https://github.com/KazumaWada/thisjobdoesnotexistyet" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+  <li>🛠️ <a href="https://thisjobdoesnotexistyet.com" target="_blank">thisjobdoesnotexistyet.com</a> ,<a href="https://github.com/KazumaWada/thisjobdoesnotexistyet" target="_blank"><i class="fa-brands fa-github"></i></a></li>
   <li>🛠️ onedayoneline.com ,<a href="https://github.com/KazumaWada/onedayoneline" target="_blank"><i class="fa-brands fa-github"></i></a></li>
   <li>☠️ file directory system ,<a href="https://github.com/KazumaWada/filedirectorysystem" target="_blank"><i class="fa-brands fa-github"></i></a></li>
   <li>☠️ blog posting service ,<a href="https://github.com/KazumaWada/myapp" target="_blank"><i class="fa-brands fa-github"></i></a></li>
-  <li>🚀 <a href="https://text-snippet-sharing.vercel.app/" target="_blank">text snippet sharing service</a>,<a href="https://github.com/KazumaWada/text_snippet_sharing" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+  <li>☠️ <a href="https://text-snippet-sharing.vercel.app/" target="_blank">text snippet sharing service</a>,<a href="https://github.com/KazumaWada/text_snippet_sharing" target="_blank"><i class="fa-brands fa-github"></i></a></li>
   <li>📝 computer science note ,<a href="https://github.com/KazumaWada/computer-science-note/tree/main/recursion" target="_blank"><i class="fa-brands fa-github"></i></a></li>
   <li>🚀 <a href="https://kazumawada.org" target="_blank">kazumawada.org</a> ,<a href="https://github.com/KazumaWada/kazumawada.github.io" target="_blank"><i class="fa-brands fa-github"></i></a></li>
 </div>
